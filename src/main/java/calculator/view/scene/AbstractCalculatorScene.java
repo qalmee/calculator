6e95be013d32331cd108284ffb8f9ba94347f400
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-03-10 15:08:16 +0700

    Menu

--- src/main/java/calculator/view/scene/AbstractCalculatorScene.java
+++ src/main/java/calculator/view/scene/AbstractCalculatorScene.java
@@ -2,12 +2,14 @@ package calculator.view.scene;
 
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static calculator.view.LanguageProperties.getProperty;
 
 abstract class AbstractCalculatorScene extends Scene {
 
@@ -23,8 +25,31 @@ abstract class AbstractCalculatorScene extends Scene {
     AbstractCalculatorScene() {
         super(new VBox());
         mainPanel = (VBox) this.getRoot();
+        setupMenu();
         setupTextField();
         setupButtons();
+    }
+
+    private void setupMenu() {
+        MenuItem menuItemCopy = new MenuItem(getProperty("abstract_calculator_scene.menu_item_copy"));
+        MenuItem menuItemPaste = new MenuItem(getProperty("abstract_calculator_scene.menu_item_paste"));
+        MenuItem menuItemHelp = new MenuItem(getProperty("abstract_calculator_scene.menu_item_help"));
+
+        MenuItem menuItemFraction = new RadioMenuItem(getProperty("abstract_calculator_scene.menu_item_mode_fraction"));
+        MenuItem menuItemComplex = new RadioMenuItem(getProperty("abstract_calculator_scene.menu_item_mode_complex"));
+        MenuItem menuItemPNumber = new RadioMenuItem(getProperty("abstract_calculator_scene.menu_item_mode_p-number"));
+
+        Menu menuFile = new Menu(getProperty("abstract_calculator_scene.menu_file"));
+        Menu menuEdit = new Menu(getProperty("abstract_calculator_scene.menu_edit"));
+        Menu menuMode = new Menu(getProperty("abstract_calculator_scene.menu_mode"));
+        Menu menuHelp = new Menu(getProperty("abstract_calculator_scene.menu_help"));
+
+        menuEdit.getItems().addAll(menuItemCopy, menuItemPaste);
+        menuMode.getItems().addAll(menuItemFraction, menuItemComplex, menuItemPNumber);
+        menuHelp.getItems().addAll(menuItemHelp);
+
+        MenuBar menuBar = new MenuBar(menuFile, menuEdit, menuMode, menuHelp);
+        mainPanel.getChildren().add(menuBar);
     }
 
     private void setupTextField() {
