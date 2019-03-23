d01011f41a81690501317dbb128eb83c6bd33109
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-03-23 18:25:04 +0700

    Config

---
+++ src/main/java/calculator/model/Config.java
@@ -0,0 +1,43 @@
+package calculator.model;
+
+import calculator.view.localization.Language;
+
+import java.util.prefs.Preferences;
+
+public class Config {
+
+    private static final String APPLICATION_NODE_NAME = "calculator";
+    private static final String LANGUAGE_KEY = "language";
+    private static final String CALCULATOR_MODE_KEY = "calculator_mode";
+    private static final String DEFAULT_LANGUAGE_KEY = "ENGLISH";
+    private static final String DEFAULT_CALCULATOR_MODE_KEY = "FRACTION";
+
+    private static Preferences preferences;
+
+    static {
+        Preferences root = Preferences.userRoot();
+        preferences = root.node(APPLICATION_NODE_NAME);
+    }
+
+    private Config() {
+
+    }
+
+    public static Language getLanguage() {
+        String language = preferences.get(LANGUAGE_KEY, DEFAULT_LANGUAGE_KEY);
+        return Language.valueOf(language);
+    }
+
+    public static void setLanguage(Language language) {
+        preferences.put(LANGUAGE_KEY, language.toString());
+    }
+
+    public static CalculatorMode getCalculatorMode() {
+        String calculatorMode = preferences.get(CALCULATOR_MODE_KEY, DEFAULT_CALCULATOR_MODE_KEY);
+        return CalculatorMode.valueOf(calculatorMode);
+    }
+
+    public static void setCalculatorMode(CalculatorMode calculatorMode) {
+        preferences.put(CALCULATOR_MODE_KEY, calculatorMode.toString());
+    }
+}
