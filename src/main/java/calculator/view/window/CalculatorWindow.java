cb8b5e2838dfd9700e50a93ab8d6bf94a72bf49d
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-03-30 20:40:04 +0700

    Сделан запуск окна по центру

--- src/main/java/calculator/view/window/CalculatorWindow.java
+++ src/main/java/calculator/view/window/CalculatorWindow.java
@@ -1,13 +1,20 @@
 package calculator.view.window;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 import static calculator.view.localization.LanguageProperties.getProperty;
 
 public class CalculatorWindow extends Stage {
 
+    private Window window;
+
     public CalculatorWindow() {
+        window = this;
         setupCalculatorWindow();
     }
 
@@ -15,5 +22,11 @@ public class CalculatorWindow extends Stage {
         this.setTitle(getProperty("calculator_window.title"));
         this.getIcons().add(new Image(getClass().getResourceAsStream("/icons/window_icon.png")));
         this.setResizable(false);
+
+        window.addEventHandler(WindowEvent.WINDOW_SHOWN, event -> {
+            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+            window.setX((screenBounds.getWidth() - window.getWidth()) / 2);
+            window.setY((screenBounds.getHeight() - window.getHeight()) / 2);
+        });
     }
 }
