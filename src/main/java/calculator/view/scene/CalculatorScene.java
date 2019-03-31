12a8802f1d3d3c8f04d3456e5101a83272ee027a
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-03-31 21:49:16 +0700

    backspace для обычных и P-ичных чисел

--- src/main/java/calculator/view/scene/CalculatorScene.java
+++ src/main/java/calculator/view/scene/CalculatorScene.java
@@ -275,7 +275,9 @@ public class CalculatorScene extends Scene implements CalculatorObserver {
 
         buttonBackSpace.setOnAction(event -> {
             String textInTextField = textFieldValue.getText();
-            //todo backspace
+            if (backSpaceEnabled) {
+                textFieldValue.setText(textInTextField.substring(0, textInTextField.length() - 1));
+            }
         });
     }
 
